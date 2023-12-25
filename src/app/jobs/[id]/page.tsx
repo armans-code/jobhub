c7f13b29b25507ed5e35795fabb4f7cd090a5975
Arman <arman.kumaraswamy@gmail.com>
2023-12-24 19:59:13 -0500

    job/[id] page & api route, simple datafetching

---
+++ src/app/jobs/[id]/page.tsx
@@ -0,0 +1,22 @@
+import { headers } from 'next/headers';
+
+const fetchData = async (id: string) => {
+  const host = headers().get('host');
+  const res = await fetch(`http://${host}/api/job/${id}`);
+  return res.json();
+};
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const { title, description, location, salary, company } = await fetchData(
+    params.id,
+  );
+  return (
+    <div>
+      <p>Title: {title}</p>
+      <p>Description: {description}</p>
+      <p>Location: {location}</p>
+      <p>Salary: {salary}</p>
+      <p>Company: {company.name}</p>
+    </div>
+  );
+}
--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,7 +1,22 @@
+import { headers } from 'next/headers';
+
+const fetchData = async () => {
+  const host = headers().get('host');
+  const res = await fetch(`http://${host}/api/job`);
+  return res.json();
+};
+
 export default async function page() {
+  const jobs = await fetchData();
+  console.log(jobs);
   return (
     <div>
-      <p>hi</p>
+      {jobs?.map((job: any) => (
+        <div key={job.id}>
+          <h1>{job.title}</h1>
+          <p>{job.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
