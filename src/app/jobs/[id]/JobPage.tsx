6dd963058a79517edfe8a33fc3be38d7975ee984
Arman <arman.kumaraswamy@gmail.com>
2023-12-26 16:53:23 -0500

    refactor: remove SWR use

--- src/app/jobs/[id]/JobPage.tsx
+++ src/app/jobs/[id]/JobPage.tsx
@@ -1,27 +1,13 @@
 'use client';
 import React from 'react';
 import ApplyForm from './ApplyForm';
-import useJob from '../../../hooks/useJob';
 
 function JobPage({ children, id }: { children: React.ReactNode; id: string }) {
-  const { data, isLoading } = useJob(id);
-
   return (
     <div>
       {children}
 
       <ApplyForm id={id} />
-
-      {isLoading ? (
-        <p>Loading applicants...</p>
-      ) : (
-        data?.applicants.map((applicant: any) => (
-          <div key={applicant.id}>
-            <p>{applicant.name}</p>
-            <p>{applicant.email}</p>
-          </div>
-        ))
-      )}
     </div>
   );
 }
