91893c9709e07e06e5156c260c53921fb1e0d35b
Arman <arman.kumaraswamy@gmail.com>
2023-12-26 21:06:04 -0500

    refactor: move /login to (pages) folder

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -3,7 +3,7 @@
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-import { Database } from '../../../lib/database.types';
+import { Database } from '../../../../lib/database.types';
 
 export default function Login() {
   const [email, setEmail] = useState('');
@@ -38,32 +38,30 @@ export default function Login() {
   };
 
   return (
-    <>
-      <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
-        <div className='bg-white shadow-md rounded px-8 py-6'>
-          <input
-            className='mb-4 py-2 px-3 border rounded'
-            name='email'
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            placeholder='Email'
-          />
-          <input
-            className='mb-4 py-2 px-3 border rounded'
-            type='password'
-            name='password'
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            placeholder='Password'
-          />
-          <button
-            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-            onClick={handleSignIn}
-          >
-            Sign in
-          </button>
-        </div>
+    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+      <div className='bg-white shadow-md rounded px-8 py-6'>
+        <input
+          className='mb-4 py-2 px-3 border rounded'
+          name='email'
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          placeholder='Email'
+        />
+        <input
+          className='mb-4 py-2 px-3 border rounded'
+          type='password'
+          name='password'
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          placeholder='Password'
+        />
+        <button
+          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+          onClick={handleSignIn}
+        >
+          Sign in
+        </button>
       </div>
-    </>
+    </div>
   );
 }
